cd08305ea6065f5900505ee01133c291bb67eee6
bangawan22 <kemas.kurniawan@radenfatah.ac.id>
2022-09-20 15:34:39 +0700

    tugas prom

--- promise.js
+++ promise.js
@@ -1,8 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+  const hasilIxx = await promiseTheaterIXX();
+  const hasilVgc = await promiseTheaterVGC();
+  const penonton = [...penontonTheaterIXX, ...penontonTheaterVGC];
 
+    const totalEmosiPenonton = groupPenonton.filter(
+      (penonton) => penonton.hasil == emosi
+    );
+    return totalEmosiPenonton.length;
+  } 
+  catch (err) {
+    console.log(err);
+  }
+};
 module.exports = {
   promiseOutput,
 };
